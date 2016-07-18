e89da701627aa8e8bafa74c5252768d5431a37e3
Jakob Krantz <ijakkra@gmail.com>
2016-07-18 15:22:21 +0200

    Bug fix

--- api/Temperature.js
+++ api/Temperature.js
@@ -33,7 +33,7 @@ module.exports = (db, app, authenticate) => {
       switch (req.query.unit) {
         case 'days':
           start.setDate(start.getDate() + 1 - req.query.count);
-          start.setHours(0);
+          start.setHours(start.getHours() - 24);
           start.setMinutes(0);
           break;
         case 'hours':
